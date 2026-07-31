f65a74e81d0ba826b4477a52b2011a4f7541e93a
tai14022006-lab <tai14022006@gmail.com>
2026-07-31 15:32:18 +0700

    Delete Repository.java

--- Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookmanagement.repository;
-
-import com.example.bookmanagement.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
