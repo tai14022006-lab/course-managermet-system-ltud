e9495cea055c41dd6dc4fee98da46a4f9a5b548d
tai14022006-lab <tai14022006@gmail.com>
2026-07-31 15:35:36 +0700

    Add files via upload

---
+++ course-managerment/src/test/java/com/course/managerment/CourseManagermentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.managerment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagermentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
